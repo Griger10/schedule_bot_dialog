e5735d3189cb0e910ddde20d6c657d7cfa5d193f
Grisha Gertzik <griger1011@gmail.com>
2024-10-04 00:45:52 +0300

    small updates

--- bot/main.py
+++ bot/main.py
@@ -1,15 +1,16 @@
+import sys
 from asyncio import WindowsSelectorEventLoopPolicy
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from config import load_config
 from aiogram import Bot, Dispatcher
 import asyncio
 
 
-
-
 async def main():
     config = await load_config()
     dp = Dispatcher()
-    bot = Bot(token='')
+    bot = Bot(token=config.bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
     await dp.start_polling(bot)
 
