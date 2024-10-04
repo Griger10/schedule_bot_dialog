61ccbc3a074b1c0ffd13610748d64f340e47fadc
Grisha Gertzik <griger1011@gmail.com>
2024-10-04 19:52:09 +0300

    added user table

--- bot/db/models/user.py
+++ bot/db/models/user.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 class User(Base):
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     telegram_id = mapped_column(BigInteger, index=True)
     username: Mapped[str]
     group: Mapped[int | None]
