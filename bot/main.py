3dee35722735267e4d04d942a38ab2ecb492df27
Grisha Gertzik <griger1011@gmail.com>
2024-10-04 19:32:15 +0300

    updates in main

--- bot/main.py
+++ bot/main.py
@@ -2,16 +2,21 @@ import sys
 from asyncio import WindowsSelectorEventLoopPolicy
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
 from bot.utils.i18n import create_translator_hub
-from config import load_config
+from config import load_config, load_database
 from aiogram import Bot, Dispatcher
 import asyncio
+from sqlalchemy.ext.asyncio import create_async_engine
 
 
 async def main():
+    storage = MemoryStorage()
     config = await load_config()
+    database_config = await load_database()
+    engine = create_async_engine(url=database_config.dsn, echo=database_config.is_echo)
     translator_hub = create_translator_hub()
-    dp = Dispatcher()
+    dp = Dispatcher(storage=storage)
     bot = Bot(token=config.bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
     print('start bot...')
