e3f3f32ce5b949d0af250ad7d95106e830071e41
Grisha Gertzik <griger1011@gmail.com>
2024-10-04 19:27:53 +0300

    env added dsn

--- bot/config.py
+++ bot/config.py
@@ -7,8 +7,21 @@ class Config:
     bot_token: str
 
 
+@dataclass
+class DatabaseConfig:
+    dsn: str
+    is_echo: bool
+
+
 async def load_config() -> Config:
     env = Env()
     env.read_env()
     bot_token = env("BOT_TOKEN")
     return Config(bot_token=bot_token)
+
+
+async def load_database() -> DatabaseConfig:
+    env = Env()
+    env.read_env()
+    dsn = env("DATABASE_DSN")
+    return DatabaseConfig(dsn=dsn, is_echo=False)
